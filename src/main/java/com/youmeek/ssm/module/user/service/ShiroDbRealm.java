5a334be0fb4b8ed05a18ee52187b7faeb5dab13d
Tinyhome <jiafabing@gmail.com>
2016-10-19 16:50:06 +0800

    2016/10/19 增加shiro相关类

---
+++ src/main/java/com/youmeek/ssm/module/user/service/ShiroDbRealm.java
@@ -0,0 +1,45 @@
+package com.youmeek.ssm.module.user.service;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by Tinyhome on 2016/10/19.
+ */
+public class ShiroDbRealm extends AuthorizingRealm {
+
+
+    protected UserService userService;
+
+    protected PermissionService permissionService;
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public PermissionService getPermissionService() {
+        return permissionService;
+    }
+
+    public void setPermissionService(PermissionService permissionService) {
+        this.permissionService = permissionService;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
---
+++ src/main/java/com/youmeek/ssm/module/user/service/UserService.java
@@ -0,0 +1,12 @@
+package com.youmeek.ssm.module.user.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Tinyhome on 2016/10/19.
+ */
+
+//Spring Service Bean的标识.
+@Component
+public class UserService {
+}
